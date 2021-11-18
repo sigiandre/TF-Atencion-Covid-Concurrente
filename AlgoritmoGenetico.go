8460019ac3b2cd6933c849fde36447da22ba67ea
Nico-j-j <nicolekuylen65@gmail.com>
2021-11-17 19:48:45 -0500

    struct

--- AlgoritmoGenetico.go
+++ AlgoritmoGenetico.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
-
+type Atencion struct {
+	id_persona            int
+	EESS                  int
+	fecha_ingreso         string
+	hora_ingreso          string
+	recuperado            bool
+	fecha_alta            string
+	recuperado_voluntario bool
+	fecha_alta_voluntaria string
+	fallecido             bool
+	fecha_fallecido       string
+	referido              bool
+	fecha_referido        string
+	EESS_destino_id       int
 }
